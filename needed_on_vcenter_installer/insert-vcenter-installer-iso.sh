fa428ea160449c43c59eb16374e933eeb599c81c
flee999 <flee999@hotmail.com>
2021-08-18 18:06:48 -0400

    insert vcenter iso to vm

--- needed_on_vcenter_installer/insert-vcenter-installer-iso.sh
+++ needed_on_vcenter_installer/insert-vcenter-installer-iso.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-vm=seedvm-192.168.140.220
+if [[ $# -lt 1 ]]; then
+  echo "Usage: <fullPathTo-VMware-VCSA.iso>"
+  echo "Example: /data/VMware-VCSA-all-6.7.0-16708996_u3j.iso"
+  exit 1
+fi
+vcsa_iso="$1"
 
-cdrom=`virsh domblklist $vm --details | grep cdrom | awk {'print $3'}`
-currentISO=`virsh domblklist $seedvm --details | grep cdrom | awk {'print $4'}`
+vm="seedvm-192.168.140.220"
 
-# eject current
+cdrom=$(virsh domblklist $vm --details | grep cdrom | awk {'print $3'})
+currentISO=$(virsh domblklist $vm --details | grep cdrom | awk {'print $4'})
+echo $vm current cdrom $cdrom with iso loaded $currentISO
+
+set -x
 virsh change-media $vm $cdrom $currentISO --eject
 
 # inject vcenter installer ISO
-virsh change-media $vm $cdrom /data/VMware-VCSA-all-6.7.0-16708996_u3j.iso --insert
+virsh change-media $vm $cdrom "$vcsa_iso" --insert
 
