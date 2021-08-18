c295ecb2f610f56ce9d5b6ffad87756ec003faf1
flee999 <flee999@hotmail.com>
2021-08-18 17:39:07 -0400

    test for basic seed connectivity

---
+++ tf-kvm-seedvm/run-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+network=140
+
+# add ssh fingerprint to known_hosts so we don't have to be asked later
+ssh-keyscan -H 192.168.$network.220 >> ~/.ssh/known_hosts
+
+# test forward+reverse DNS resolution
+set -ex
+ssh -i id_rsa ubuntu@192.168.$network.220 "dig esxi1.home.lab +short; dig -x 192.168.140.236 +short; dig vcenter.home.lab +short; dig -x 192.168.140.237 +short"
+
+# test outside ping to archive.ubuntu.com apt repository
+# fails if upstream router for this network not configured with static route for subnet
+set -ex
+ssh -i id_rsa ubuntu@192.168.$network.220 "dig archive.ubuntu.com +short; ping -c1 archive.ubuntu.com"
+set +ex
+
+echo ""
+echo ""
+echo "DONE. All tests ran successfully"
+
