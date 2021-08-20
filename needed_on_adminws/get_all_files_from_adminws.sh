7a1adfa3d8794e4f76042539623a0c7a98ada672
flee999 <flee999@hotmail.com>
2021-08-20 06:50:59 -0400

    k9s installer

--- needed_on_adminws/get_all_files_from_adminws.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#scp -i gke-admin-workstation ubuntu@192.168.124.221:* .
-scp -i gke-admin-workstation ubuntu@192.168.124.221:/tmp/anthos.home.lab.* .
-
--- needed_on_adminws/k8s/install-k9s.sh
+++ needed_on_adminws/k8s/install-k9s.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo apt install jq -y
+curver=$(curl -sL https://api.github.com/repos/derailed/k9s/releases/latest | jq -r ".tag_name")
+wget https://github.com/derailed/k9s/releases/download/$curver/k9s_Linux_x86_64.tar.gz
+
 tar xvfz k9s_Linux_x86_64.tar.gz
+rm LICENSE README.md
+
 sudo cp k9s /usr/local/bin/.
 sudo chmod +x /usr/local/bin/k9s
