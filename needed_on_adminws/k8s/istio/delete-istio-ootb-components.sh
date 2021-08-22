83d5828d76ff5a8d6a6b0437d6d59309586dc123
Fabian Lee <flee999@hotmail.com>
2021-08-22 15:55:36 -0400

    put delay between deletions

--- needed_on_adminws/k8s/istio/delete-istio-ootb-components.sh
+++ needed_on_adminws/k8s/istio/delete-istio-ootb-components.sh
@@ -9,6 +9,8 @@ kubectl get hpa -n gke-system
 kubectl delete hpa/istio-ingress-hpa -n gke-system
 kubectl delete hpa/istio-pilot-hpa -n gke-system
 
+sleep 10
+
 # show pod disruption budgets
 kubectl get pdb -n gke-system
 
@@ -16,13 +18,19 @@ kubectl get pdb -n gke-system
 kubectl delete pdb/istio-ingress -n gke-system
 kubectl delete pdb/istio-pilor -n gke-system
 
+sleep 10
+
 # delete deployments
 kubectl delete deployment/istio-pilot -n gke-system
 kubectl delete deployment/istio-ingress -n gke-system
 
+sleep 10
+
 # delete services
 kubectl delete service/istio-pilot -n gke-system
 kubectl delete service/istio-ingress -n gke-system
+
+sleep 10
 
 # show components now
 kubectl get all -n gke-system
@@ -36,11 +44,15 @@ kubectl get sa -n gke-system
 kubectl delete sa/istio-ingressgateway-service-account -n gke-system
 kubectl delete sa/istio-pilot-service-account -n gke-system
 
+sleep 10
+
 # now delete all secrets starting with 'istio'
 for name in $(kubectl get secrets -n gke-system -o=jsonpath="{.items[*].metadata.name}"); do [[ $name != istio* ]] || kubectl delete secret/$name -n gke-system; done
 
 # show secrets
 kubectl get secrets -n gke-system
+
+sleep 10
 
 # delete namespace, if it gets stuck empty the finalizers array of the namespace
 kubectl delete ns gke-system
