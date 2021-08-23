a7d04e372f437d6dbc203af4093baac44c5f13ef
Fabian Lee <flee999@hotmail.com>
2021-08-23 08:01:05 -0400

    refactor for istio ootb (out-of-the-box)

--- needed_on_adminws/k8s/install-k9s.sh
+++ needed_on_adminws/k8s/install-k9s.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# docker ppa changed its label, apt can fix this (apt-get cannot)
+sudo apt update -y
+
 sudo apt install jq -y
 curver=$(curl -sL https://api.github.com/repos/derailed/k9s/releases/latest | jq -r ".tag_name")
 wget https://github.com/derailed/k9s/releases/download/$curver/k9s_Linux_x86_64.tar.gz
