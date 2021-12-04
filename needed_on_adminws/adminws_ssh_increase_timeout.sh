1f99fc2864495590858d3e24577198ae7dde7499
Fabian Lee <flee999@hotmail.com>
2021-12-03 22:12:02 -0500

    set ssh timeout per docs

--- needed_on_adminws/adminws_ssh_increase_timeout.sh
+++ needed_on_adminws/adminws_ssh_increase_timeout.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# https://cloud.google.com/anthos/clusters/docs/on-prem/1.8/known-issues#ssh_connection_closed_by_remote_host
 
 echo "Increasing timeout for Admin Workstation so that long-running gkectl commands do not timeout"
 sudo sed -i 's/.*ClientAliveInterval.*/ClientAliveInterval 1200/' /etc/ssh/sshd_config
-sudo sed -i 's/.*ClientAliveCountMax.*/ClientAliveCountMax 3/' /etc/ssh/sshd_config
+sudo sed -i 's/.*ClientAliveCountMax.*/ClientAliveCountMax 9/' /etc/ssh/sshd_config
+sudo grep Alive /etc/ssh/sshd_config
+
 sudo systemctl reload sshd
 echo "DONE"
