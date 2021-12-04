e453868d0da290deab4eee569b017e8fc4563004
Fabian Lee <flee999@hotmail.com>
2021-12-04 12:50:48 -0500

    using 'mycluster1' now for vcenter

--- needed_on_vcenter_installer/copy_to_seedvm.sh
+++ needed_on_vcenter_installer/copy_to_seedvm.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#
+# copy files over to seedVM
+#
 
+# copy json file for vcenter setup
 for file in 'vcsa-esxi.json' ; do
   scp -i ../tf-kvm-seedvm/id_rsa $file ubuntu@192.168.140.220:.
 done
+
+# copy govc related files
 scp -i ../tf-kvm-seedvm/id_rsa -r ../govc $file ubuntu@192.168.140.220:.
