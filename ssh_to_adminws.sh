1994f622a1de0cc7c92ef71ee756b2fff9b25caa
Fabian Lee <flee999@hotmail.com>
2022-05-13 09:09:27 -0400

    allow call from other dir

--- ssh_to_adminws.sh
+++ ssh_to_adminws.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && (pwd -W 2> /dev/null || pwd))
 
 set -x
-ssh -i $SCRIPT_DIR/needed_on_adminws/gke-admin-workstation ubuntu@192.168.140.221
+ssh -o ServerAliveInterval=30 -i $SCRIPT_DIR/needed_on_adminws/gke-admin-workstation ubuntu@192.168.140.221
