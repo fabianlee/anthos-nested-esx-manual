2f476e932ae7ca51a82e3b074400c2e3d0aa0568
Fabian Lee <flee999@hotmail.com>
2022-10-18 05:25:47 -0400

    needed role/compute.viewer added to component-access service account

--- needed_on_seedvm/set-roles-component-access-sa.sh
+++ needed_on_seedvm/set-roles-component-access-sa.sh
@@ -10,6 +10,7 @@ accountEmail=$(gcloud iam service-accounts list --project=$projectId --filter=$n
 gcloud projects add-iam-policy-binding $projectId --member "serviceAccount:$accountEmail" --role "roles/serviceusage.serviceUsageViewer"
 gcloud projects add-iam-policy-binding $projectId --member "serviceAccount:$accountEmail" --role "roles/iam.roleViewer"
 gcloud projects add-iam-policy-binding $projectId --member "serviceAccount:$accountEmail" --role "roles/iam.serviceAccountCreator"
+# new as of 1.13, validation does region listing that fails without this IAM role
 gcloud projects add-iam-policy-binding $projectId --member "serviceAccount:$accountEmail" --role "roles/compute.viewer"
 
 # was not asked for in 1.4 docs, but am having problems with back-off pulling image during admin cluster creation so trying this additional role
